b63a8ad2bcd13d26eb992214a060158a58ea557f
Anjukpaul <95705730+Anjukpaul@users.noreply.github.com>
2022-04-20 14:41:42 +0530

    Add files via upload

---
+++ Sort.java
@@ -0,0 +1,19 @@
+import java.util.Arrays
+public class sort{
+public static void main(String args[]{
+int n1;
+Scanner read= new Scanner(System.in);
+System.out.println("enter the size of thea array;");
+n1=read.nextInt();
+System.out.println("enter the value:")
+String[] arr=new String[n1];
+for (int i=0;i<n1;i++)
+{
+arr[i]=read.next[]);
+}
+Array.sort(arr);
+for(int i=0;i<n1;i++)
+{
+System.out.println(arr[i]+,"");
+}
+} 
